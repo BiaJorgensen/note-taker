f295a8a955588a8366f64234800bc80d86a7bfda
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-02-26 16:06:33 -0600

    Created api/notes route to read from db.json

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const api = require('../Develop/routes/notes')
 
 
 app.use(express.static('public'));
+app.use(api)
 
 
 
