72eb3bf434eedc0ddb1b3fe0d8c6643e1fa13387
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-02-26 15:52:20 -0600

    Changed path to main page to wildcard

--- Develop/server.js
+++ Develop/server.js
@@ -5,14 +5,16 @@ const app = express();
 
 app.use(express.static('public'));
 
-// GET Route for main page
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for main page (using wildcard)
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(3000)
