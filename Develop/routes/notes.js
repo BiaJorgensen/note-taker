6cd86badef2d31604ee1a91156a09cb8099130ee
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-02-26 18:39:51 -0600

    Added response to post; changed get to read db.json using fs

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -5,7 +5,11 @@ const { v4: uuidv4 } = require('uuid');
 const fs = require('fs')
 
 
-router.get('/api/notes', (req, res) => res.json(notes));
+router.get('/api/notes', (req, res) => {
+    const readNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    res.json(readNotes)
+});
+
 
 router.post('/api/notes', (req, res) => {
     const { title, text } = req.body;
@@ -17,10 +21,11 @@ router.post('/api/notes', (req, res) => {
     }
 
     const readNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
-    readNotes.push(newNote)
-    const noteString = JSON.stringify(readNotes)
+    readNotes.push(newNote);
+    const noteString = JSON.stringify(readNotes);
 
-    fs.writeFileSync('./db/db.json', noteString)
+    fs.writeFileSync('./db/db.json', noteString);
+    res.json(newNote)
 
 } )
 
