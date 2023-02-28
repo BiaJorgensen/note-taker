7bdab0733902edff08afcc87a0ed5c5c30a9785c
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-02-27 18:16:25 -0600

    Added comments

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,44 +1,57 @@
+// Importing Router method from Express.js
 const router = require('express').Router();
-// const path = require('path');
-const notes = require('../db/db.json');
+// Importing uuid library that generates unique identifiers
 const { v4: uuidv4 } = require('uuid');
+// Importing File System
 const fs = require('fs')
 
-
+// GET Route for endpoint /api/notes
 router.get('/api/notes', (req, res) => {
+    // Reading db.json file
     const readNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    // Returning saved notes as JSON
     res.json(readNotes)
 });
 
-
+// POST Route for endpoint /api/notes
 router.post('/api/notes', (req, res) => {
     const { title, text, id } = req.body;
+    // Generating unique indentifier
     const generateId = uuidv4()
-
+    // When POST is called, it creates a new object with title, text and unique ID
     const newNote = {
         title,
         text,
         id: generateId
     }
-
+    // Reading db.json file
     const readNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    // Pushing the new note into array with notes already in db.json
     readNotes.push(newNote);
     const noteString = JSON.stringify(readNotes);
-
+    // Saving all notes in db.json
     fs.writeFileSync('./db/db.json', noteString);
+    // Returning saved notes as JSON
     res.json(newNote)
 
 } );
 
+// DELETE Route for endpoint /api/notes
 router.delete('/api/notes/:id', (req, res) => {
+    // Reading db.json file
     const readNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    // Filtering notes to only show the ones that are not being deleted (don't have the ID of the deleted note)
     const notDeletedNotes = readNotes.filter(note => note.id !== req.params.id)
     const noteString = JSON.stringify(notDeletedNotes);
+    // Saving remaining notes in db.json
     fs.writeFileSync('./db/db.json', noteString);
+    // Returning ID of the deleted note
     res.json(`${req.params.id} has been deleted`)
     
     
 })
+
+// Exporting router
 
 module.exports = router
 
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,9 @@
+// Importing Express.js
 const express = require('express');
+// Importing Node.js path module
 const path = require('path');
 const app = express();
+// Importing notes.js
 const api = require('../Develop/routes/notes');
 
 // Middleware for parsing JSON and urlencoded form data
@@ -23,5 +26,6 @@ app.get('/*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// Server listening on port 3000
 app.listen(3000)
 
